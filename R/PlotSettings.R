6ae99f8df88f767c54a036eaa6c54e5f7c041c45
DaniFlat <70110340+DaniFlat@users.noreply.github.com>
2026-02-10 16:40:15 +0100

    Add files via upload

---
+++ R/PlotSettings.R
@@ -0,0 +1,49 @@
+library("ggplot2")
+
+#font
+t <- list(
+  family = "Helvetica",
+  size = 18,
+  face = "bold",
+  color = 'black')
+
+fontfamily <- "Helvetica"
+
+#xaxis plotly layout
+xaxis <- list(
+  showgrid = FALSE,
+  showline = TRUE,
+  linewidth = 2,
+  linecolor='black',
+  mirror = TRUE,
+  ticks = "outside",
+  zeroline = FALSE,
+  font = t
+)
+
+#yaxis plotly layout
+yaxis <- list(
+  mirror = TRUE,
+  linewidth = 2,
+  tickwidth = 2,
+  linecolor='black',
+  tickformat = "digits",
+  showgrid = FALSE,
+  zeroline = FALSE,
+  showline = TRUE,
+  font = t,
+  ticks = "outside",
+  hoverformat = '.2f')
+
+#ggplot theme
+ggplottheme <- theme(axis.text.x=element_text(colour="black", face = "bold", family = fontfamily, size = 4.76 * .pt),
+                    axis.text.y=element_text(colour="black", face = "bold", family = fontfamily),
+                    axis.title.x = element_text(family = fontfamily),
+                    axis.title.y = element_text(family = fontfamily),
+                    axis.ticks = element_line(size = 2, color="black"),
+                    axis.ticks.length=unit(.2, "cm"),
+                    legend.title=element_text(face = "bold", family = fontfamily, size=5.81 * .pt),
+                    legend.text=element_text(face = "bold.italic", family = fontfamily, size=4.76 * .pt),
+                    legend.margin = margin(0, 0, 0, 0),
+                    legend.spacing.y = unit(0, "mm")
+                    )
